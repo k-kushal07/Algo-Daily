6530048fb248d01426bd7ed9d6d3eff8ae8bc307
Kushal Kapoor <60167153+k-kushal07@users.noreply.github.com>
2021-02-23 00:03:42 +0530

    Update Day 32 - Fibonacci Sequence.py

--- Day 32 - Fibonacci Sequence.py
+++ Day 32 - Fibonacci Sequence.py
@@ -8,7 +8,7 @@ def fibo(num):
     elif(num == 1): 
         return b 
     else: 
-        for i in range(2,num): 
+        for i in range(2,num+1): 
             c = a + b 
             a = b 
             b = c 

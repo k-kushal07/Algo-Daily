7b4bad69e0b39000c15d0f47d11abad3f0d24ac3
Kushal Kapoor <60167153+k-kushal07@users.noreply.github.com>
2021-02-22 23:59:09 +0530

    Create Day 32 - Fibonacci Sequence.py

---
+++ Day 32 - Fibonacci Sequence.py
@@ -0,0 +1,18 @@
+def fibo(num): 
+    a = 0
+    b = 1
+    if(num < 0): 
+        print("Invalid Number") 
+    elif(num == 0): 
+        return a 
+    elif(num == 1): 
+        return b 
+    else: 
+        for i in range(2,num): 
+            c = a + b 
+            a = b 
+            b = c 
+        return b 
+
+num = int(input('Enter the number: '))
+print(fibo(num))
